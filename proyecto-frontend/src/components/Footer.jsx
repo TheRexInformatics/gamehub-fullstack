15772d489900674981877ef7d653b7e47f11fc2f
TheRexInformatics <ma.villarroelt@duocuc.cl>
2025-12-16 20:01:57 -0300

    Actualizacion Footer

--- proyecto-frontend/src/components/Footer.jsx
+++ proyecto-frontend/src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <footer className="footer">
       <div className="footer-content">
         {/* El símbolo de copyright funciona igual en JSX */}
-        <p>&copy; 2025 GameHub. Todos los derechos reservados a TheRexInformatics</p>
+        <p>&copy; 2025 GameHub. Todos los derechos reservados.</p>
         <div className="footer-links">
           <a href="#">Términos de Servicio</a>
           <a href="#">Política de Privacidad</a>
